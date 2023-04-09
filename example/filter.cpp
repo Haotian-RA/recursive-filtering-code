9b57c7a00c1b08da15256003a32c08831cfd4849
Haotian-RA <81209069+Haotian-RA@users.noreply.github.com>
2023-04-08 20:43:00 -0400

    Update filter.cpp

--- example/filter.cpp
+++ example/filter.cpp
@@ -1,8 +1,3 @@
-/*    
-    recommand compiler flag: 
-    clang++ -I/usr/local/include -mavx2 -mfma -march=native -fno-trapping-math -fno-math-errno -I$VCL_PATH -std=c++20 -O3 -o filter filter.cpp
-*/
-
 #include "recursive_filter.h"
 #include <chrono>
 #include <iostream>
